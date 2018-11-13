6f3ce1b6c9070d3a675b9de2c8523df327bc83e9
Bassel Ahmed <bassel.ahmed@outlook.com>
2018-11-13 09:19:32 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app"><HeaderCmp /></div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderCmp from "./components/Header.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HeaderCmp
   }
 };
 </script>
+
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+@import url('~@/assets/reset.css');
+
+*, *::after, *::before {
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+  font-family: lato, sans-serif;
+  color: #1a1a1a;
+  background-color: #fff;
+}
+
+a {
+  color: #db6356;
+  text-decoration: none;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: teal;
+  min-height: 100vh;
+}
 </style>
